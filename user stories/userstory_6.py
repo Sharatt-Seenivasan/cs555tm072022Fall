02248c8b6aa39a209eda0b7e18361b77e82ce942
aghani416416 <114269979+aghani416416@users.noreply.github.com>
2022-10-03 16:26:34 -0400

    divorce before death

---
+++ user stories/userstory_6.py
@@ -0,0 +1,24 @@
+#User Story 6
+
+from datetime import datetime
+from datetime import date
+import pandas as pd
+from dateutil.relativedelta import relativedelta
+import math
+
+def greaterDate(first_date, second_date):
+    if datetime.strptime(first_date, " %d %b %Y") > datetime.strptime(second_date, " %d %b %Y"):
+        return True
+    return False
+
+def divorceBeforeDeath(individuals, families):
+    for index, row in families.iterrows():
+        divorceDate = row['divorce']
+        husband_death = individuals.loc[individuals['id'] == row['Husband ID'], 'death'].iloc[0]
+        wife_death = individuals.loc[individuals['id'] == row['Wife ID'], 'death'].iloc[0]
+
+        if greaterDate(husband_death, divorceDate) == False:
+            print("ERROR: Husband death date before divorce.")
+
+        if greaterDate(wife_death, divorceDate) == False:
+            print("Error: Wife death date before divorce.")
