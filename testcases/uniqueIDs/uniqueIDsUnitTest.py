d315ba6740b7a448ee1e7419411b1787a960b283
Jason Wong <jasonw5172001@gmail.com>
2022-10-03 19:05:38 -0400

    Finished unittests

--- testcases/uniqueIDs/uniqueIDsUnitTest.py
+++ testcases/uniqueIDs/uniqueIDsUnitTest.py
@@ -1,25 +1,40 @@
+import io
 import os
 import unittest
+from unittest.mock import patch
 from uniqueIDs import unique_ids
 
 class TestStringMethods(unittest.TestCase):
 
-    filename_dir = os.path.dirname(os.path.abspath(__file__))
+    @patch('builtins.print')
+    def test_file_1(self, mock_print):
+        filename1 = os.path.dirname(os.path.abspath(__file__)) + '/uniqueIDsTestData1.xlsx'
+        unique_ids(filename1)
+        mock_print.assert_called_with('ID I3 is not a unique ID.')
 
-    def test_file_1(self):
-        self.assertEqual()
+    @patch('builtins.print')
+    def test_file_2(self, mock_print):
+        filename2 = os.path.dirname(os.path.abspath(__file__)) + '/uniqueIDsTestData2.xlsx'
+        unique_ids(filename2)
+        mock_print.assert_called_with('ID F6 is not a unique ID.')
 
-    filename1 = filename_dir + '/uniqueIDsTestData1.xlsx'
-    filename2 = filename_dir + '/uniqueIDsTestData2.xlsx'
-    filename3 = filename_dir + '/uniqueIDsTestData3.xlsx'
-    filename4 = filename_dir + '/uniqueIDsTestData4.xlsx'
-    filename5 = filename_dir + '/uniqueIDsTestData5.xlsx'
+    @patch('builtins.print')
+    def test_file_3(self, mock_print):
+        filename3 = os.path.dirname(os.path.abspath(__file__)) + '/uniqueIDsTestData3.xlsx'
+        unique_ids(filename3)
+        mock_print.assert_called_with('ID F4 is not a unique ID.\nID F5 is not a unique ID.')
 
-    unique_ids(filename1)
-    unique_ids(filename2)
-    unique_ids(filename3)
-    unique_ids(filename4)
-    unique_ids(filename5)
+    @patch('builtins.print')
+    def test_file_4(self, mock_print):
+        filename4 = os.path.dirname(os.path.abspath(__file__)) + '/uniqueIDsTestData4.xlsx'
+        unique_ids(filename4)
+        mock_print.assert_called_with('ID I7 is not a unique ID.\nID I8 is not a unique ID.')
+
+    @patch('builtins.print')
+    def test_file_5(self, mock_print):
+        filename5 = os.path.dirname(os.path.abspath(__file__)) + '/uniqueIDsTestData5.xlsx'
+        unique_ids(filename5)
+        mock_print.assert_called_with('File has all unique IDs.')
 
 if __name__ == '__main__':
     unittest.main()
--- user_stories/uniqueIDs.py
+++ user_stories/uniqueIDs.py
@@ -7,9 +7,14 @@ def check_id(id_list):
     c1 = Counter(id_list)
     c2 = Counter(set(id_list))
     diff = c1-c2
-    diff_list = set(diff.elements())
+    diff_list = sorted(set(diff.elements()))
+    
+    return_string = ""
     for x in diff_list:
-        print("ID", x, "is not a unique ID.")
+        return_string += ("ID " + x + " is not a unique ID.\n")
+    if return_string != "":
+        print(return_string[:-1])
+
     return len(diff_list) == 0
 
 def unique_ids(filename):
@@ -22,9 +27,12 @@ def unique_ids(filename):
 
     has_unique_individuals = check_id(individual_id_list)
     has_unique_families = check_id(family_id_list)
+    all_unique = has_unique_individuals and has_unique_families
 
-    if (has_unique_individuals and has_unique_families):
+    if all_unique:
         print("File has all unique IDs.")
+    
+    return all_unique
 
 #filename = os.path.abspath(os.path.dirname(__file__)) + '/../test_data.xlsx'
 #unique_ids(filename)
