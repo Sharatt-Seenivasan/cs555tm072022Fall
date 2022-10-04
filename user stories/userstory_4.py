d568b5fee7d807ca8d0143dafae67810861fa72b
Sharattram-Seenivasan <49329786+Sharattram-Seenivasan@users.noreply.github.com>
2022-10-03 22:19:49 -0400

    rewrote marriage_before_divorce to output error message with more detail

--- user stories/userstory_4.py
+++ user stories/userstory_4.py
@@ -2,32 +2,16 @@ import pandas as pd
 import parser
 import math
 
-def marriage_before_divorce(gedcom_file):
-    filename = 'test_data.ged'
-    writer = pd.ExcelWriter('out.xlsx')
-    (individuals,individuals_id_and_name) = parser.save_ind_data(filename)
-    families = parser.save_family_data(filename, individuals_id_and_name)
-    pd.DataFrame(individuals).to_excel(writer, sheet_name="Individuals")
-    pd.DataFrame(families).sort_values(by = ['id']).to_excel(writer, sheet_name="Families")
-    writer.save()
-    df=pd.read_excel('out.xlsx')
-    xls = pd.ExcelFile('out.xlsx')
-    df1=pd.read_excel(xls,'Individuals')
-    df2=pd.read_excel(xls,'Families')
+def marriage_before_divorce(families_dataframe, family_id_indices):
     error_type = "US04 Marriage before divorce"
-    xls = pd.ExcelFile('out.xlsx')
-    df2=pd.read_excel(xls,'Families')
+    df2=families_dataframe
     for row in df2.itertuples(index=False):
         if not type(row.married)==float and not type(row.divorced)==float:
             marr = row.married
             divv = row.divorced
             if marr > divv:
-                message = "Divorced date "+str(divv)+", Before Marriage date "+str(marr)+ " , therefore it is not valid."
-                #save_invalidfor_print(res["FAMID"], "US04", message)
-                #print(message)  
-                return False
-            
-    return True  
+                message = "ERROR: FAMILY: US03: " + row.id + ": Divorced date "+str(divv)+", Before Marriage date "+str(marr)+ " , therefore it is not valid."
+                print(message)    
                 
 
     
