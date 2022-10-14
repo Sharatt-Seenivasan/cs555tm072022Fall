56d00cf4a9fd50408c9ca499cd9d80aa5bc3386d
Sharattram-Seenivasan <49329786+Sharattram-Seenivasan@users.noreply.github.com>
2022-10-14 16:08:43 -0400

    added user stories 9 and 26

---
+++ user_stories/userstory09.py
@@ -0,0 +1,26 @@
+import io
+import unittest
+import unittest.mock
+import pandas as pd
+import os
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+from datetime import date
+
+def greaterDate(first_date, second_date):
+    if datetime.strptime(first_date, " %d %b %Y") > datetime.strptime(second_date, " %d %b %Y"):
+        return True
+    return False
+
+def areBirthsBeforeParentDeaths(individuals_dataframe, family_dataframe):
+  for index, row in family_dataframe.iterrows():
+    husband_death = individuals_dataframe.loc[individuals_dataframe['id'] == row['Husband ID'], 'death'].iloc[0]
+    wife_death = individuals_dataframe.loc[individuals_dataframe['id'] == row['Wife ID'], 'death'].iloc[0]
+    if(str(row['children'])!='nan'):
+      for child_id in row['children']:
+        child_birthday = individuals_dataframe.loc[individuals_dataframe['id'] == child_id, 'birthday'].iloc[0]
+        if str(child_birthday) != 'nan':
+          if str(husband_death) != 'nan' and greaterDate(str(child_birthday),str(husband_death)):
+            print("ERROR: FAMILY: US09: Husband " + row['Husband ID'] + " in Family " + row['id'] + " dies before Child " + child_id + " is born.")
+          if str(wife_death) != 'nan' and greaterDate(str(child_birthday),str(wife_death)):
+            print("ERROR: FAMILY: US09: Wife " + row['Wife ID'] + " in Family " + row['id'] + " dies before Child " + child_id + " is born.")
