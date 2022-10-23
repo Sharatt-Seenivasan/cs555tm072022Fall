0efb9b2060096eedfb99b0f93239344a49fcf1b9
Sharattram-Seenivasan <49329786+Sharattram-Seenivasan@users.noreply.github.com>
2022-10-23 11:14:56 -0400

    Added compareDates function

--- user_stories/userStory3/userstory_3.py
+++ user_stories/userStory3/userstory_3.py
@@ -1,6 +1,14 @@
 import pandas as pd
 import parser
 import math
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+from datetime import date
+
+def compareDates(date_one, date_two):
+  if datetime.strptime(date_one," %d %b %Y") > datetime.strptime(date_two," %d %b %Y"):
+    return True
+  return False
 
 def birth_before_death(individuals_dataframe):
     df = individuals_dataframe
