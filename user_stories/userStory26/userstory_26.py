082b0732e7efe74c6f6ce7f8a0a6a19a5bf8653f
Sharattram-Seenivasan <49329786+Sharattram-Seenivasan@users.noreply.github.com>
2022-10-23 12:34:11 -0400

    added conditional that checks if child is in list of children

--- user_stories/userStory26/userstory_26.py
+++ user_stories/userStory26/userstory_26.py
@@ -11,7 +11,9 @@ def hasCorrespondingEntries(individuals_dataframe, family_dataframe):
   for index, row in individuals_dataframe.iterrows():
     if str(row['child']) != 'nan':
       children = family_dataframe.loc[family_dataframe['id'] == row['child'], 'children'].iloc[0]
-      if row['id'] not in children:
+      if row['id'] != row['id']:
+        print("ERROR: INDIVIDUAL: US26: " + row['id'] + " not in Family " +  row['child'] + " as a child.")
+      elif row['id'] not in children:
         print("ERROR: INDIVIDUAL: US26: " + row['id'] + " not in Family " +  row['child'] + " as a child.")
     if str(row['spouse']) != 'nan':
       husband_id = family_dataframe.loc[family_dataframe['id'] == row['spouse'], 'Husband ID'].iloc[0]
@@ -21,11 +23,12 @@ def hasCorrespondingEntries(individuals_dataframe, family_dataframe):
 
 
   for index, row in family_dataframe.iterrows():
-    if row['Husband ID'] not in individuals_dataframe['id'].values:
+    if row['Husband ID'] == row['Husband ID'] and row['Husband ID'] not in individuals_dataframe['id'].values:
       print("ERROR: FAMILY: US26: Husband " + row['Husband ID'] + " not in table of individuals")
-    if row['Wife ID'] not in individuals_dataframe['id'].values:
+    if row['Wife ID'] == row['Wife ID'] and row['Wife ID'] not in individuals_dataframe['id'].values:
       print("ERROR: FAMILY: US26: Wife " + row['Wife ID'] + " not in table of individuals")  
     if str(row['children']) != 'nan':
       for child in row['children']:
         if child not in individuals_dataframe['id'].values:
            print("ERROR: FAMILY: US26: Child " + child + " not in table of individuals")
+
