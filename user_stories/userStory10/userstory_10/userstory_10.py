305a3882bd6d3aa29e9a346cff1cdacd7a2eb495
Sharattram-Seenivasan <49329786+Sharattram-Seenivasan@users.noreply.github.com>
2022-10-23 12:05:25 -0400

    Added error message

--- user_stories/userStory10/userstory_10/userstory_10.py
+++ user_stories/userStory10/userstory_10/userstory_10.py
@@ -1,3 +1,4 @@
+import pandas
 from datetime import *
 from dateutil.relativedelta import *
 
@@ -6,26 +7,31 @@ def marriage_after_14(individuals, families):
     all_legal_marriages = True
 
     for index, row in families.iterrows():
-        marriage_time = datetime.strptime(row['married'], " %d %b %Y")
+        if row['married'] == row['married']:
+            marriage_time = datetime.strptime(row['married'], " %d %b %Y")
 
-        #error check if husband in individuals does not exist
-        husband_birth = individuals.loc[individuals['id'] == row['Husband ID'], 'birthday'].iloc[0]
-        husband_birth_time = datetime.strptime(husband_birth, " %d %b %Y")
-        husband_marriage_age = relativedelta(marriage_time, husband_birth_time).years
+            #error check if husband in individuals does not exist
+            if row['Husband ID'] in individuals['id'].values: 
+                husband_birth = individuals.loc[individuals['id'] == row['Husband ID'], 'birthday'].iloc[0]
+                husband_birth_time = datetime.strptime(husband_birth, " %d %b %Y")
+                husband_marriage_age = relativedelta(marriage_time, husband_birth_time).years
 
-        #error check if wife in individuals does not exist
-        wife_birth = individuals.loc[individuals['id'] == row['Wife ID'], 'birthday'].iloc[0]
-        wife_birth_time = datetime.strptime(wife_birth, " %d %b %Y")
-        wife_marriage_age = relativedelta(marriage_time, wife_birth_time).years
+                if row['Wife ID'] in individuals['id'].values: 
+                    wife_birth = individuals.loc[individuals['id'] == row['Wife ID'], 'birthday'].iloc[0]
+                    wife_birth_time = datetime.strptime(wife_birth, " %d %b %Y")
+                    wife_marriage_age = relativedelta(marriage_time, wife_birth_time).years
 
-        if husband_marriage_age < legal_marriage_age:
-            print("ERROR: FAMILY: US10: " + row['Husband ID'] + " from family " + row['id'] + " married before the age of 14!")
-            all_legal_marriages = False
-        if wife_marriage_age < legal_marriage_age:
-            print("ERROR: FAMILY: US10: " + row['Wife ID'] + " from family " + row['id'] + " married before the age of 14!")
-            all_legal_marriages = False
+                if husband_marriage_age < legal_marriage_age and wife_marriage_age < legal_marriage_age:
+                    print("ERROR: FAMILY: US10: Both spouses from family " + row['id'] + " married before the age of 14!")
+                    all_legal_marriages = True
+                elif husband_marriage_age < legal_marriage_age:
+                    print("ERROR: FAMILY: US10: Husband from family " + row['id'] + " married before the age of 14!")
+                    all_legal_marriages = True
+                elif wife_marriage_age < legal_marriage_age:
+                    print("ERROR: FAMILY: US10: Wife from family " + row['id'] + " married before the age of 14!")
+                    all_legal_marriages = True
 
-    if all_legal_marriages:
-        print("File has all marriages after 14.", wife_birth)
+    #if all_legal_marriages:
+        #print("File has all marriages after 14.")
 
     return all_legal_marriages
