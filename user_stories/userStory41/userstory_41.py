920657cd3fdf8a72fba3e07a40cf2ae26558ff17
Sharattram-Seenivasan <49329786+Sharattram-Seenivasan@users.noreply.github.com>
2022-10-23 12:37:09 -0400

    added str output to user story 41

--- user_stories/userStory41/userstory_41.py
+++ user_stories/userStory41/userstory_41.py
@@ -5,11 +5,13 @@ from dateutil.relativedelta import *
 def include_partial_dates(date):
     listDate = date.split()
     if len(listDate) == 3:
-        datetime_object = datetime.strptime(date, " %d %b %Y")
+        str_date = date
     if len(listDate) == 2:
-        datetime_object = datetime.strptime(date, " %b %Y")
+        str_date = ' 1 ' + listDate[0] + " " + listDate[1]
+        print("US41: Partial date " + str(listDate) + " converted into usable format.")
     if len(listDate) == 1:
-        datetime_object = datetime.strptime(date, " %Y")
-    
-    print (datetime_object)
-    return datetime_object
+        str_date = ' 1 ' + 'JAN ' + listDate[0]
+        print("US41: Partial date " + str(listDate) + " converted into usable format.")
+    return str_date
+
+include_partial_dates(" 28 JUN 2018")
