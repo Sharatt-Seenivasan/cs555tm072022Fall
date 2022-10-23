ea4fd1c2a8f1a58d32d4cb8af192710973c61bba
Sharattram-Seenivasan <49329786+Sharattram-Seenivasan@users.noreply.github.com>
2022-10-23 12:35:40 -0400

    added user story number to error message

--- user_stories/userStory27/userstory_27.py
+++ user_stories/userStory27/userstory_27.py
@@ -1,18 +1,12 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[3]:
-
-
 import pandas as pd
 import parser
 import math
     
 def individual_ages(individuals_dataframe):
-    for i in range(len(individuals_dataframe)):
-        name=individuals_dataframe[i]["name"]
-        age=individuals_dataframe[i]["age"]
-        message= "Name:" + name.replace("/"," ")+ "--->" + "Age:" +str(age)
-        #print(message)
-    return message
+    for index,row in individuals_dataframe.iterrows():
+        name=row['name']
+        age=row["age"]
+        if age < 0 or math.isnan(age):
+            message= "ERROR: INDIVIDUAL: US27: " + name + " does not have a valid age."
+            print(message)
 
