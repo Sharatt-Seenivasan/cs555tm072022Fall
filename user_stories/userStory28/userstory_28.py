9e337576bb8aef99eb10b6c54c642181294e3344
Sharattram-Seenivasan <49329786+Sharattram-Seenivasan@users.noreply.github.com>
2022-10-28 15:52:21 -0400

    Update userstory_28.py

--- user_stories/userStory28/userstory_28.py
+++ user_stories/userStory28/userstory_28.py
@@ -1,10 +1,13 @@
 import pandas as pd
 
-def sortChildrenByAge(children, individual_dataframe):
-    child_ages = []
-    for child in children:
-        age = individual_dataframe.loc[individual_dataframe['id'] == child][0]
-        child_ages.append((child,age))
-    child_ages.sort(key = lambda x : x[1])
-    sorted_children = [child in (child,ages) in child_ages]
-    return sorted_children
+def listChildrenByAge(individual_dataframe,families_dataframe):
+    print("US28: Children in each family ordered by age.")
+    for index, row in families_dataframe.iterrows():
+        print("Children in Family " + row['id'] + ":") 
+        child_ages = []
+        for child in row['children']:
+            age = individual_dataframe.loc[individual_dataframe['id'] == child, 'age'].iloc[0]
+            child_ages.append((child,age))
+        child_ages.sort(key = lambda x : x[1], reverse=True)
+        sorted_children = [child for (child,age) in child_ages]
+        print(sorted_children)
