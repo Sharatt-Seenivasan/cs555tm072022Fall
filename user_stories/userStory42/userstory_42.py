11ac87acba1e7b3c9991e107df3e8c86486a60dd
Sharattram-Seenivasan <49329786+Sharattram-Seenivasan@users.noreply.github.com>
2022-10-30 19:40:31 -0400

    added index to error message

--- user_stories/userStory42/userstory_42.py
+++ user_stories/userStory42/userstory_42.py
@@ -2,12 +2,12 @@ from datetime import *
 from dateutil.relativedelta import *
 
 
-def reject_illegitimate_dates(date):
+def reject_illegitimate_dates(date, index):
     date_format = " %d %b %Y"
 
     try:
         date_object = datetime.strptime(date, date_format)
         #print(date_object)
     except:
-        print ("Incorrect date format at <insert ID here>")
+        print ("ERROR: INDIVIDUAL: US42: Input Line # " + str(index) + " Incorrect date format")
 
