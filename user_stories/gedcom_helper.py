f9345ef6bef023b23a7a74dc91697c86f62bfb2e
Jason Wong <jasonw5172001@gmail.com>
2022-10-30 19:18:34 -0400

    Moved gedcom_helper to user_stories directory
    To make importing easier

--- user_stories/gedcom_helper.py
+++ user_stories/gedcom_helper.py
@@ -208,7 +208,6 @@ def getIDIndices(filename):
   f.close()
   return indices
 
-
 def getFamilyIndices(filename):
   indices = {}
   f = open(filename, "r")
@@ -221,138 +220,16 @@ def getFamilyIndices(filename):
   f.close()
   return indices
 
-#deprecated
-def save_ind_data(filename):
-    individuals = []
-    individuals_id_and_name = {}
-    f = open(filename, "r")
-    lines = f.readlines()
+def file_parser(ged_filename):
+    (individuals, individuals_id_and_name) = createIndDataframe(ged_filename)
+    families = createFamilyDataframe(ged_filename, individuals_id_and_name)
+    individuals = pd.DataFrame(individuals)
+    families = pd.DataFrame(families)
+    return (individuals, families)
 
-    indices = []
-    for i in range(0,len(lines)):
-        line = lines[i]
-        level = line[0]
-        if 'INDI' in line.split() and level == '0':
-            indices.append(i)
-
-    for i in indices:
-        is_dead = False
-        person = {}
-        if indices.index(i) != len(indices)-1:
-            for j in range(i,indices[indices.index(i)+1]):
-                line = lines[j]
-                level = line[0]
-                if 'INDI' in line.split() and level == '0':
-                    person['id'] = line.split()[1].replace('@','')
-                if 'NAME' in line.split():
-                    person['name'] = line.split('NAME')[1].replace('\n','')
-                    individuals_id_and_name[person['id']] = person['name']
-                if 'SEX' in line.split() and level == '1':
-                    person['gender'] = line.split()[2]
-                if 'BIRT' in line.split():
-                    person['birthday'] =  lines[j+1].split('DATE')[1].replace('\n','')
-                if 'DEAT' in line.split():
-                    is_dead = True
-                    person['death'] = lines[j+1].split('DATE')[1].replace('\n','')
-                if 'FAMC' in line.split():
-                    person['child'] = line.split()[2].replace('@','')
-                if 'FAMS' in line.split():
-                    person['spouse'] = line.split()[2].replace('@','')
-        else:
-            for j in range(i,len(lines)-1):
-                line = lines[j]
-                level = line[0]
-                if 'INDI' in line.split() and level == '0':
-                    person['id'] = line.split()[1].replace('@','')
-                if 'NAME' in line.split():
-                    person['name'] = line.split('NAME')[1].replace('\n','')
-                    individuals_id_and_name[person['id']] = person['name']
-                if 'SEX' in line.split() and level == '1':
-                    person['gender'] = line.split()[2]
-                if 'BIRT' in line.split():
-                    person['birthday'] = lines[j+1].split('DATE')[1].replace('\n','')
-                if 'DEAT' in line.split():
-                    is_dead = True
-                    person['death'] = lines[j+1].split('DATE')[1].replace('\n','')
-                if 'FAMC' in line.split():
-                    person['child'] = line.split()[2].replace('@','')
-                if 'FAMS' in line.split():
-                    person['spouse'] = line.split()[2].replace('@','')
-
-        if is_dead:
-            person['age'] = relativedelta(pd.to_datetime(person['death'],infer_datetime_format=True), pd.to_datetime(person['birthday'],infer_datetime_format=True)).years
-        else:
-            today = date.today()
-            person['age'] = relativedelta(today, pd.to_datetime(person['birthday'],infer_datetime_format=True)).years
-
-        individuals.append(person)
-
-    f.close()
-        
-    return (individuals,individuals_id_and_name)
-
-#deprecated
-def save_family_data(filename, individuals_id_and_name):
-    families = []
-    f = open(filename, "r")
-    lines = f.readlines()
-
-    indices = []
-    for i in range(0,len(lines)):
-        line = lines[i]
-        level = line[0]
-        if 'FAM' in line.split() and level == '0':
-            indices.append(i)
-    
-    for i in indices:
-        family = {}
-        if indices.index(i) != len(indices)-1:
-            for j in range(i,indices[indices.index(i)+1]):
-                line = lines[j]
-                level = line[0]
-                if 'FAM' in line.split() and level == '0':
-                    family['id'] = line.split()[1].replace('@','')
-                if 'MARR' in line.split():
-                    family['married'] = lines[j+1].split('DATE')[1].replace('\n','')
-                if 'DIV' in line.split():
-                    family['divorced'] = lines[j+1].split('DATE')[1].replace('\n','')
-                if 'HUSB' in line.split():
-                    family['Husband ID'] = line.split()[2].replace('@','')
-                    family['Husband Name'] = individuals_id_and_name[family['Husband ID']]
-                if 'WIFE' in line.split():
-                    family['Wife ID'] = line.split()[2].replace('@','')
-                    family['Wife Name'] = individuals_id_and_name[family['Wife ID']]
-                if 'CHIL' in line.split():
-                    if 'children' in family: 
-                        family['children'].append(line.split()[2].replace('@',''))
-                    else:
-                        family['children'] = []
-                        family['children'].append(line.split()[2].replace('@',''))
-        else:
-            for j in range(i,len(lines)-1):
-                line = lines[j]
-                level = line[0]
-                if 'FAM' in line.split() and level == '0':
-                    family['id'] = line.split()[1].replace('@','')
-                if 'MARR' in line.split():
-                    family['married'] = lines[j+1].split('DATE')[1].replace('\n','')
-                if 'DIV' in line.split():
-                    family['divorced'] = lines[j+1].split('DATE')[1].replace('\n','')
-                if 'HUSB' in line.split():
-                    family['Husband ID'] = line.split()[2].replace('@','')
-                    family['Husband Name'] = individuals_id_and_name[family['Husband ID']]
-                if 'WIFE' in line.split():
-                    family['Wife ID'] = line.split()[2].replace('@','')
-                    family['Wife Name'] = individuals_id_and_name[family['Wife ID']]
-                if 'CHIL' in line.split():
-                    if 'children' in family: 
-                        family['children'].append(line.split()[2].replace('@',''))
-                    else:
-                        family['children'] = []
-                        family['children'].append(line.split()[2].replace('@',''))
-
-        families.append(family)
-
-    f.close()
-
-    return families
+def output_data(individuals, families, ged_filename):
+    output_excel = ged_filename.strip('.ged') + '.xlsx'
+    with pd.ExcelWriter(output_excel) as writer:
+        individuals.to_excel(writer, sheet_name="Individuals")
+        families.sort_values(by = ['id']).to_excel(writer, sheet_name="Families")
+    return output_excel
---
+++ user_stories/userStory30/userstory_30_unittest.py
@@ -0,0 +1,18 @@
+import os
+import unittest
+from unittest.mock import patch
+from userstory_30 import *
+from user_stories import file_parser, output_data
+
+class TestStringMethods(unittest.TestCase):
+
+    @patch('builtins.print')
+    def test_file_1(self, mock_print):
+        filename1 = os.path.dirname(os.path.abspath(__file__)) + '/userstory_18_testdata1.ged'
+        individuals, families = file_parser(filename1)
+        output = output_data(individuals, families, filename1)
+        list_living_married(individuals, families)
+        mock_print.assert_called_with('ERROR: FAMILY: US18: F2 is a sibling marriage sharing the same parents!')
+
+if __name__ == '__main__':
+    unittest.main()
