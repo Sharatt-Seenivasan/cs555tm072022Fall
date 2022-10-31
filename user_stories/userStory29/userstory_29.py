29176b2cedab3eb5b7c72c9662cc51fb262a5c5d
Sharattram-Seenivasan <49329786+Sharattram-Seenivasan@users.noreply.github.com>
2022-10-31 18:27:18 -0400

    added age for userstory27

--- user_stories/userStory29/userstory_29.py
+++ user_stories/userStory29/userstory_29.py
@@ -2,5 +2,5 @@ import pandas as pd
 
 def getDeadPeople(individuals_dataframe):
     print("US29: List of dead people")
-    deadPeople = [(row['id'], row['name']) for index, row in individuals_dataframe.iterrows() if not row['alive']]
+    deadPeople = [(row['id'], row['age']) for index, row in individuals_dataframe.iterrows() if not row['alive']]
     print(deadPeople)
