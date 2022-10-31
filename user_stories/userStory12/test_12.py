410884095787dd5cf098d5cc00f2db466a43ad6c
kavya2304 <kavyasri.talluri@gmail.com>
2022-10-31 00:45:01 -0400

    corrected the test cases

--- user_stories/userStory12/test_12.py
+++ user_stories/userStory12/test_12.py
@@ -13,32 +13,15 @@ import pytest
 import pandas as pd
 
 def test_parents_not_too_old():
-    person_details = [{'id': 'I2',
-  'name': ' Steven /Brindley/',
-  'gender': 'M',
-  'birthday': ' 2 JUL 1969',
-  'spouse': 'F2',
-  'child': 'F3',
-  'age': 83},{'id': 'I7',
-  'name': ' Lisa /Rutherford/',
-  'gender': 'F',
-  'birthday': ' 5 MAR 1990',
-  'spouse': 'F2',
-  'age': 72},{'id': 'I8',
-  'name': ' Reginald /Brindley/',
-  'birthday': ' 10 AUG 2017',
-  'child': 'F2',
-  'age': 5},]
-    family_details=[{'id': 'F2',
-  'Husband ID': 'I2',
-  'Husband Name': ' Steven /Brindley/',
-  'Wife ID': 'I7',
-  'Wife Name': ' Lisa /Rutherford/',
-  'children': ['I8'],
-  'married': ' 12 JUL 2016'},]
-    df1=pd.DataFrame(person_details)
-    df2=pd.DataFrame(family_details)
     
-    strout= "ERROR:USERSTORY12 parents are too old"
-    print(person_details)
-    assert strout == parents_not_too_old(df1,df2)
+    testData = "export-BloodTree.ged"
+    (individuals,individuals_id_and_name) = createIndDataframe(testData)
+    families = createFamilyDataframe(testData, individuals_id_and_name)
+    strout="ERROR: FAMILY: US12: : F6000000188481846843 Age of child: 5 Age of mother: 32 Age of Father: 53"
+    str1=parents_not_too_old(individuals,families,getFamilyIndices(testData))
+    print(str1)
+    assert strout==parents_not_too_old(individuals,families,getFamilyIndices(testData))
+    
+
+
+#test_parents_not_too_old()
