a64a8d4f1abf415ed8b5d0cd48680afb6f511bd8
Sharattram-Seenivasan <49329786+Sharattram-Seenivasan@users.noreply.github.com>
2022-10-31 16:18:41 -0400

    commented out print statement for name and age

--- user_stories/userStory27/userstory_27.py
+++ user_stories/userStory27/userstory_27.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import parser
 import math
     
 def individual_ages(individuals_dataframe,id_indices):
@@ -8,10 +9,7 @@ def individual_ages(individuals_dataframe,id_indices):
         if age < 0 or math.isnan(age):
             message="ERROR: INDIVIDUAL: US27: " + str(id_indices[row['id']]) + ": " + row['id'] + " Name: " + name + " does not have a valid age."
             print(message)
-        else:
-            print("NAME:"+ name +",Age:" + str(age))
+        #else:
+            #print("NAME:"+ name +",Age:" + str(age))
 
-            
-#(individuals,individuals_id_and_name) = createIndDataframe("test_data.ged")
-#individual_ages(individuals,getIDIndices("test_data.ged"))
 
