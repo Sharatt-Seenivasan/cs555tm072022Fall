94a08fbf4926d2818baf9f7f26bd7cf428237c37
Sharattram-Seenivasan <49329786+Sharattram-Seenivasan@users.noreply.github.com>
2022-10-31 14:52:44 -0400

    added nan check

--- user_stories/userStory17/userstory_17.py
+++ user_stories/userStory17/userstory_17.py
@@ -13,12 +13,10 @@ def no_marriages_to_descendants(families,family_id_indices):
                     husbandid=row1['Husband ID']
                     if child ==wifeid or child==husbandid:
                         ids_of_desc=row1.children
-                        if row['Wife ID']==ids_of_desc[0] or row['Husband ID']==ids_of_desc[0]:
-                            message="ERROR: FAMILY: US17: " + str(family_id_indices[row['id']]) + ": " + row['id'] + " Id of child: " + str(ids_of_desc[0]) + " ,Wife ID: " + str(row['Wife ID'])+ " ,Husband ID: "+ str(row['Husband ID'])
-                            print(message)
+                        if str(ids_of_desc) != 'nan':
+                            if row['Wife ID']==ids_of_desc[0] or row['Husband ID']==ids_of_desc[0]:
+                                message="ERROR: FAMILY: US17: " + str(family_id_indices[row['id']]) + ": " + row['id'] + " Id of child: " + str(ids_of_desc[0]) + " ,Wife ID: " + str(row['Wife ID'])+ " ,Husband ID: "+ str(row['Husband ID'])
+                                print(message)
     return message
 
-(individuals,individuals_id_and_name) = createIndDataframe("test_data.ged")
-families = createFamilyDataframe("test_data.ged", individuals_id_and_name)
-no_marriages_to_descendants(families,getFamilyIndices("test_data.ged"))
 
