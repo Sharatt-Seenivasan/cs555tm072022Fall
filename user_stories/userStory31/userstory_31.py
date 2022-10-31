79acdfec33d6f45c5b30de427605c712d1e1ccec
Sharattram-Seenivasan <49329786+Sharattram-Seenivasan@users.noreply.github.com>
2022-10-31 18:22:16 -0400

    added ages for userstory27

--- user_stories/userStory31/userstory_31.py
+++ user_stories/userStory31/userstory_31.py
@@ -2,10 +2,9 @@ import os
 import pandas as pd
 
 def list_living_single(individuals, families):
-    individuals_list = list(individuals['id'])
-    living_over30_people = [row['id'] for index,row in individuals.iterrows() if row['alive'] and row['age'] > 30]
-    single_people = [row['id'] for index,row in individuals.iterrows() if str(row['spouse'])=='nan']
+    living_over30_people = [(row['id'], row['age']) for index,row in individuals.iterrows() if row['alive'] and row['age'] > 30]
+    single_people = [(row['id'], row['age']) for index,row in individuals.iterrows() if str(row['spouse'])=='nan']
 
     living_single = [x for x in living_over30_people if x in single_people]
 
-    print("US31: The list of living people that are single: " + str(living_single)
+    print("US31: The list of living people that are single and over 30: " + str(living_single))
