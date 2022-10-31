b08de1d542a29462679507fe362419254d7aff1f
Sharattram-Seenivasan <49329786+Sharattram-Seenivasan@users.noreply.github.com>
2022-10-31 13:26:00 -0400

    added indices to error messages

--- user_stories/userStory26/userstory_26.py
+++ user_stories/userStory26/userstory_26.py
@@ -12,21 +12,21 @@ def hasCorrespondingEntries(individuals_dataframe, family_dataframe):
     if str(row['child']) != 'nan':
       children = family_dataframe.loc[family_dataframe['id'] == row['child'], 'children'].iloc[0]
       if row['id'] != row['id']:
-        print("ERROR: INDIVIDUAL: US26: " + row['id'] + " not in Family " +  row['child'] + " as a child.")
+        print("ERROR: INDIVIDUAL: US26: Input Line # " + str(row['index']) + ": " + row['id'] + " not in Family " +  row['child'] + " as a child.")
       elif row['id'] not in children:
-        print("ERROR: INDIVIDUAL: US26: " + row['id'] + " not in Family " +  row['child'] + " as a child.")
+        print("ERROR: INDIVIDUAL: US26: " + str(row['index']) + ": " + row['id'] + " not in Family " +  row['child'] + " as a child.")
     if str(row['spouse']) != 'nan':
       husband_id = family_dataframe.loc[family_dataframe['id'] == row['spouse'], 'Husband ID'].iloc[0]
       wife_id = family_dataframe.loc[family_dataframe['id'] == row['spouse'], 'Wife ID'].iloc[0]
       if row['id'] != husband_id and row['id'] != wife_id:
-        print("ERROR: INDIVIDUAL: US26: " + row['id'] + " not in Family " +  row['spouse'] + " as a spouse.")
+        print("ERROR: INDIVIDUAL: US26: " + str(row['index']) + ": " + row['id'] + " not in Family " +  row['spouse'] + " as a spouse.")
 
 
   for index, row in family_dataframe.iterrows():
     if row['Husband ID'] == row['Husband ID'] and row['Husband ID'] not in individuals_dataframe['id'].values:
-      print("ERROR: FAMILY: US26: Husband " + row['Husband ID'] + " not in table of individuals")
+      print("ERROR: FAMILY: US26: Input Line # " + str(row['index']) + " Husband " + row['Husband ID'] + " not in table of individuals")
     if row['Wife ID'] == row['Wife ID'] and row['Wife ID'] not in individuals_dataframe['id'].values:
-      print("ERROR: FAMILY: US26: Wife " + row['Wife ID'] + " not in table of individuals")  
+      print("ERROR: FAMILY: US26: Input Line # " + str(row['index']) + " Wife " + row['Wife ID'] + " not in table of individuals")  
     if str(row['children']) != 'nan':
       for child in row['children']:
         if child not in individuals_dataframe['id'].values:
