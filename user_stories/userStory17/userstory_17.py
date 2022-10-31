2638fa5a60c4428b2f453d8046512057b5663fc4
Sharattram-Seenivasan <49329786+Sharattram-Seenivasan@users.noreply.github.com>
2022-10-31 16:08:49 -0400

    reworked user story 17 so it detects descendants further down the tree

--- user_stories/userStory17/userstory_17.py
+++ user_stories/userStory17/userstory_17.py
@@ -1,21 +1,27 @@
 import pandas as pd
+import parser
 import math
 
-def no_marriages_to_descendants(families,family_id_indices):
-    message="there are no such marriages"
+def no_marriages_to_descendants(individuals,families,family_id_indices):
     for index, row in families.iterrows():
-        if not type(row.children)==float or not math.isnan(row.children):
-            ids_of_children=row.children
-            for child in row.children:
-                for index,row1 in families.iterrows():
-                    wifeid= row1['Wife ID']
-                    husbandid=row1['Husband ID']
-                    if child ==wifeid or child==husbandid:
-                        ids_of_desc=row1.children
-                        if str(ids_of_desc) != 'nan':
-                            if row['Wife ID']==ids_of_desc[0] or row['Husband ID']==ids_of_desc[0]:
-                                message="ERROR: FAMILY: US17: " + str(family_id_indices[row['id']]) + ": " + row['id'] + " Id of child: " + str(ids_of_desc[0]) + " ,Wife ID: " + str(row['Wife ID'])+ " ,Husband ID: "+ str(row['Husband ID'])
-                                print(message)
-    return message
+        wife_id = row['Wife ID']
+        husband_id = row['Husband ID']
+        if husband_id in getDescendants(wife_id,individuals,families):
+            print("ERROR: FAMILY: US17: Line Index # " + str(family_id_indices[row['id']]) + ": " + " Wife " + wife_id + " in Family " + row['id'] + " is married to their descendant!")
+        if wife_id in getDescendants(husband_id,individuals,families):
+            print("ERROR: FAMILY: US17: Line Index # " + str(family_id_indices[row['id']]) + ": " + " Husband " + husband_id_id + " in Family " + row['id'] + " is married to their descendant!")
 
-
+def getDescendants(person_id, individuals_dataframe, families_dataframe):
+    descendants = []
+    spouse_type = ''
+    if individuals_dataframe.loc[individuals_dataframe['id'] == person_id, 'gender'].iloc[0] == 'M':
+        spouse_type = 'Husband ID'
+    else:
+        spouse_type = 'Wife ID'
+    for index, row in families_dataframe.iterrows():
+        if row[spouse_type] == person_id:
+            if str(row['children']) != 'nan':
+                for child in row['children']:
+                    descendants.append(child)
+                    descendants.extend(getDescendants(child,individuals_dataframe,families_dataframe))
+    return descendants
