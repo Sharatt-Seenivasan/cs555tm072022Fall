5420b42bba87bedb05846a3983395b1a7edbbad8
Sharattram-Seenivasan <49329786+Sharattram-Seenivasan@users.noreply.github.com>
2022-10-31 14:42:16 -0400

    added nan check

--- user_stories/userStory28/userstory_28.py
+++ user_stories/userStory28/userstory_28.py
@@ -5,9 +5,10 @@ def listChildrenByAge(individual_dataframe,families_dataframe):
     for index, row in families_dataframe.iterrows():
         print("Children in Family " + row['id'] + ":") 
         child_ages = []
-        for child in row['children']:
-            age = individual_dataframe.loc[individual_dataframe['id'] == child, 'age'].iloc[0]
-            child_ages.append((child,age))
+        if str(row['children']) != 'nan':
+            for child in row['children']:
+                age = individual_dataframe.loc[individual_dataframe['id'] == child, 'age'].iloc[0]
+                child_ages.append((child,age))
         child_ages.sort(key = lambda x : x[1], reverse=True)
         sorted_children = [child for (child,age) in child_ages]
         print(sorted_children)
