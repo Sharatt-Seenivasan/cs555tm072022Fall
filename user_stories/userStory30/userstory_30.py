5f7bf9a2b65816d236db71eae64cb1344c36eced
Sharattram-Seenivasan <49329786+Sharattram-Seenivasan@users.noreply.github.com>
2022-10-31 18:24:03 -0400

    added ages for userstory 27

--- user_stories/userStory30/userstory_30.py
+++ user_stories/userStory30/userstory_30.py
@@ -2,8 +2,8 @@ import pandas as pd
 
 def list_living_married(individuals, families):
     individuals_list = list(individuals['id'])
-    living_people = [row['id'] for index,row in individuals.iterrows() if row['alive']]
-    married_people = [row['id'] for index,row in individuals.iterrows() if row['spouse']]
+    living_people = [(row['id'], row['age']) for index,row in individuals.iterrows() if row['alive']]
+    married_people = [(row['id'], row['age']) for index,row in individuals.iterrows() if row['spouse']]
     living_married = [x for x in living_people if x in married_people]
 
     print("US30: The list of living people that are married: " + str(living_married))
