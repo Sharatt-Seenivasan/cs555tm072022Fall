2302963de13b4aba5c63dc5b23494ba21e2206a3
Sharattram-Seenivasan <49329786+Sharattram-Seenivasan@users.noreply.github.com>
2022-10-31 16:47:15 -0400

    updated to work with new dataframe structure

--- user_stories/userStory30/userstory_30.py
+++ user_stories/userStory30/userstory_30.py
@@ -1,11 +1,9 @@
-import numpy as np
 import pandas as pd
 
 def list_living_married(individuals, families):
     individuals_list = list(individuals['id'])
     living_people = [row['id'] for index,row in individuals.iterrows() if row['alive']]
-    married_people = [row['id'] for index,row in individuals.iterrows() if np.all(pd.notna(row['spouse']))]
-
+    married_people = [row['id'] for index,row in individuals.iterrows() if row['spouse']]
     living_married = [x for x in living_people if x in married_people]
 
     print("US30: The list of living people that are married: " + str(living_married))
