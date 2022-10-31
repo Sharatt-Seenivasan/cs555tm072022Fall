15eb25c635b27c0988d6d295d96f7480abe2e650
Sharattram-Seenivasan <49329786+Sharattram-Seenivasan@users.noreply.github.com>
2022-10-31 16:52:32 -0400

    fixed user story printing nothing every time

--- user_stories/userStory31/userstory_31.py
+++ user_stories/userStory31/userstory_31.py
@@ -1,11 +1,11 @@
-import numpy as np
+import os
 import pandas as pd
 
 def list_living_single(individuals, families):
     individuals_list = list(individuals['id'])
     living_people = [row['id'] for index,row in individuals.iterrows() if row['alive']]
-    single_people = [row['id'] for index,row in individuals.iterrows() if np.all(pd.isna(row['spouse']))]
+    single_people = [row['id'] for index,row in individuals.iterrows() if str(row['spouse'])=='nan']
 
     living_married = [x for x in living_people if x in single_people]
 
-    print("US31: The list of living people that are married: " + str(living_married))
+    print("US31: The list of living people that are single: " + str(living_married))
