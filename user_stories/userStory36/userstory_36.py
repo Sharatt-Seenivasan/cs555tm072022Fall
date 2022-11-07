16e8da9c160f05fe659d6b8a5622bfb6fe1ecef8
kavya2304 <kavyasri.talluri@gmail.com>
2022-11-07 11:34:00 -0500

    added userstory 36

---
+++ user_stories/userStory36/userstory_36.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import pandas as pd
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+from datetime import date
+import math
+
+def listRecentDeaths(individuals):
+    current= datetime.now()
+    recentdeathlist=[]
+    for index,row in individuals.iterrows():
+        if not type(row.death)==float or not math.isnan(row.death):
+            deathDay= datetime.strptime(row.death," %d %b %Y")
+            if (current.year - deathDay.year == 0 ):
+                if ((current - deathDay).days < 30):
+                    name=row["name"].strip("/")
+                    deathdate= str(row["death"])
+                    list1.append("US:36 List of recent deaths: "+name +" "+str(deathdate))
+    return recentdeathlist
+
