708b08857f270345c3f66303fceaf4174e05fd21
aghani416416 <ahmedghani117@gmail.com>
2022-11-27 11:51:14 -0500

    Create userstory_35.py
    Recent births list

---
+++ user_stories/userStory35/userstory_35.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+from datetime import date
+import math
+
+def listRecentBirths(individuals):
+    current = datetime.now()
+    recentBirthsList = []
+    for index,row in individuals.iterrows():
+        if not type(row.birthday) == float or not math.isnan(row.birthday):
+            birthday = datetime.strptime(row.birthday, " %d %b %Y")
+            if (current.year - birthday.year == 0):
+                if ((current - birthday).days < 30):
+                    name = row["name"].strip("/")
+                    birthDate = str(row["birthday"])
+                    recentBirthsList.append("US:35 List of recent births: " + name + " " + str(birthDate))
+    return recentBirthsList
