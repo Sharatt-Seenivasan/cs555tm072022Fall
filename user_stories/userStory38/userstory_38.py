3d20f76d530e4e82cf6a7033261d7eb102cd50a5
Sharattram-Seenivasan <49329786+Sharattram-Seenivasan@users.noreply.github.com>
2022-11-28 14:26:11 -0500

    fixed bug where userstory 38 did not list upcoming birthdays

--- user_stories/userStory38/userstory_38.py
+++ user_stories/userStory38/userstory_38.py
@@ -5,17 +5,13 @@ from datetime import date
 import math
 
 def listUpComingBirthdays(individuals):
-    current= datetime.now()
-    upcomingbirthdays=[]
-    for index,row in individuals.iterrows():
-        if not type(row.birthday)==float or not math.isnan(row.birthday):
-            birthday= datetime.strptime(row.birthday," %d %b %Y")
-            if ((birthday.month-current.month) ==0 or (birthday.month-current.month)==1 ):
-                if((birthday.day-current.day)<30):
-                    name=''.join(row["name"].split('/'))
-                    age=row["age"]
-                    birthday= str(row["birthday"])
-                    upcomingbirthdays.append("US:38 List of upcoming birthdays:" +name +" : "+ "age:"+str(age) +str(birthday))
-                    print(upcomingbirthdays)
-    return upcomingbirthdays 
-
+    today = datetime.today()
+    birthdayList = []
+    for index, row in individuals.iterrows():
+        if str(row['birthday']) != 'nan':
+            birthday = datetime.strptime(row['birthday']," %d %b %Y")
+            birthDate = datetime(today.year, birthday.month, birthday.day)
+            if abs((today-birthDate).days) <= 30:
+                birthdayList.append((row['id'],row['age']))
+    print("US38: List of Upcoming Birthdays:")
+    print(birthdayList)
