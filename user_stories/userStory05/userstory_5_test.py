7d1990709cc30d5cd28406b51e0cd696a25ff574
Jason Wong <jasonw5172001@gmail.com>
2022-11-28 23:59:41 -0500

    Update userstory_5_test.py

--- user_stories/userStory05/userstory_5_test.py
+++ user_stories/userStory05/userstory_5_test.py
@@ -1,7 +1,7 @@
 import os
 import sys
 import unittest
-from userStory_5 import *
+from userstory_5 import *
 from unittest.mock import patch
 sys.path.append('')
 from gedcom_helper import file_parser, output_data
@@ -10,7 +10,10 @@ class TestStringMethods(unittest.TestCase):
 
     @patch('builtins.print')
     def test_marriage_before_death1(self, mock_print):
-        marriageBeforeDeath(gedcom_file)
+        filename = os.path.dirname(__file__) + '/../../test_data.ged'
+        individuals, families = file_parser(filename)
+        output = output_data(individuals, families, filename)
+        marriageBeforeDeath(individuals, families)
         mock_print.assert_called_with("ERROR: US 06: Husband ID: Marriage before Death ")
 
 
